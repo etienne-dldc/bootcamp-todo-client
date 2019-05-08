57b59fef8808e35959978fe31f5ecf5402be312c
Etienne Dldc <e.deladonchamps@gmail.com>
2019-05-08 11:13:35 +0200

    load todos

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,50 @@
 import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return <div>Hello React</div>;
+const wait = time => {
+  return new Promise(resolve => {
+    window.setTimeout(resolve, time);
+  });
+};
+
+class App extends React.Component {
+  state = {
+    todos: null,
+    todosLoading: true,
+  };
+
+  render() {
+    if (this.state.todosLoading === true) {
+      return <div>Fetching todos...</div>;
+    }
+    if (this.state.todos === null) {
+      return (
+        <div>
+          <p>Todos not loaded !</p>
+        </div>
+      );
+    }
+    return (
+      <div>
+        <ul>
+          {this.state.todos.map(todo => {
+            return (
+              <li key={todo.id}>
+                {todo.done ? '✅' : '❎'} {todo.name}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
+
+  async componentDidMount() {
+    await wait(2000);
+    axios.get('https://dldc-todo-server.herokuapp.com/').then(response => {
+      this.setState({ todos: response.data, todosLoading: false });
+    });
+  }
 }
 
 export default App;
